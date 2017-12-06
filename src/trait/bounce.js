160f4fe88736872f0ea2f9d5a8144e2300f8daea
julien jamet <jamet.julien@gmail.com>
2017-12-06 06:38:26 +0100

    collider adjust

--- src/trait/bounce.js
+++ src/trait/bounce.js
@@ -22,26 +22,64 @@ export default class Bounce extends Trait{
 
       if (this.distance(candidate.pos, entity.pos) <= len) {
 
-        if (Math.sign(candidate.mover.vel.x) != Math.sign(entity.mover.vel.x)) {
+        if ( Math.sign( candidate.mover.vel.x) != Math.sign(entity.mover.vel.x)) {
           entity.mover.vel.x *= -1;
           candidate.mover.vel.x *= -1;
 
-          if (Math.sign(candidate.mover.vel.x) > 0) {
+          if (Math.sign( candidate.mover.vel.x) > 0) {
             candidate.bound.left = entity.bound.right;
-          } else if (Math.sign(candidate.mover.vel.x) < 0) {
+          } else if ( Math.sign( candidate.mover.vel.x) < 0) {
             candidate.bound.right = entity.bound.left;
+          }
+
+        }else{
+          
+          if ((candidate.bound.top > entity.bound.top &&
+              candidate.bound.top < entity.bound.bottom) ||
+              (candidate.bound.bottom < entity.bound.bottom &&
+              candidate.bound.bottom > entity.bound.top)){
+
+                if ( candidate.bound.left > entity.bound.left &&
+                    candidate.bound.left < entity.bound.right){
+                  candidate.bound.left = entity.bound.left;
+                  candidate.mover.vel.x *= -1;
+                }
+
+                if (candidate.bound.right < entity.bound.right && candidate.bound.right > entity.bound.left) {
+                  candidate.bound.right = entity.bound.right;
+                  candidate.mover.vel.x *= -1;
+                }
           }
 
         }
 
-        if (Math.sign(candidate.mover.vel.y) != Math.sign(entity.mover.vel.y)) {
+        if (Math.sign( candidate.mover.vel.y) != Math.sign(entity.mover.vel.y)) {
           entity.mover.vel.y *= -1;
           candidate.mover.vel.y *= -1;
 
-          if (Math.sign(candidate.mover.vel.y) > 0) {
+          if (Math.sign( candidate.mover.vel.y) > 0) {
             candidate.bound.top = entity.bound.bottom;
-          } else if (Math.sign(candidate.mover.vel.y) < 0) {
+          } else if ( Math.sign( candidate.mover.vel.y) < 0) {
             candidate.bound.bottom = entity.bound.top;
+          }
+
+        }else{
+
+          if ((candidate.bound.left > entity.bound.left &&
+            candidate.bound.left < entity.bound.right) ||
+            (candidate.bound.right < entity.bound.right &&
+              candidate.bound.right > entity.bound.left)) {
+
+            if (candidate.bound.top > entity.bound.top &&
+              candidate.bound.top < entity.bound.bottom) {
+              candidate.bound.top = entity.bound.top;
+              candidate.mover.vel.y *= -1;
+            }
+
+            if (candidate.bound.bottom < entity.bound.bottom && candidate.bound.bottom > entity.bound.top) {
+              candidate.bound.bottom = entity.bound.bottom;
+              candidate.mover.vel.y *= -1;
+            }
           }
 
         }
